ad4cbada52b6e2f36829a54845f3b45c9d38a138
Complexlity <nwalozielijah@gmail.com>
2025-04-30 07:56:54 +0100

    feat: update config

--- contract/hardhat.config.ts
+++ contract/hardhat.config.ts
@@ -1,8 +1,21 @@
-import type { HardhatUserConfig } from "hardhat/config";
+import { type HardhatUserConfig, vars } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox-viem";
+import './tasks'
+
+const BASE_SEPOLIA_PRIVATE_KEY = vars.get("BASE_SEPOLIA_PRIVATE_KEY");
+const ALCHEMY_API_KEY = vars.get('ALCHEMY_API_KEY')
 
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
+  networks: {
+    baseSepolia: {
+      url: `https://base-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+      accounts: [BASE_SEPOLIA_PRIVATE_KEY]
+    }
+    
+  }
 };
+
+
 
 export default config;
