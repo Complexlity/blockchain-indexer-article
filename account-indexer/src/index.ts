532e8af9a3dc0fa6c1ac11445b0f349594d7dd0a
Complexlity <nwalozielijah@gmail.com>
2025-05-07 12:43:56 +0100

    chore: start script

--- account-indexer/src/index.ts
+++ account-indexer/src/index.ts
@@ -1,9 +1,7 @@
 import fs from 'fs';
 import { supportedChains } from './config';
-import { getBalanceHistory, getFilteredUserTransactions } from './queries-new';
-// const ADDRESS = "0xD4416b13d2b3a9aBae7AcD5D6C2BbDBE25686401" //Dummy Address
-// const ADDRESS = "0xe06Dacf8a98CBbd9692A17fcb8e917a6cb5e65ED"
-const ADDRESS = "0x8ff47879d9eE072b593604b8b3009577Ff7d6809"
+import { getBalanceHistory, getFilteredUserTransactions } from './queries';
+const ADDRESS = "0xD4416b13d2b3a9aBae7AcD5D6C2BbDBE25686401" //Dummy Address
 
 
 const filteredUserTransactions = await getFilteredUserTransactions(ADDRESS, supportedChains.base.moralisChain)
@@ -12,6 +10,8 @@ const balanceHistory = await getBalanceHistory(ADDRESS, filteredUserTransactions
 if (!fs.existsSync('./data')) {
     fs.mkdirSync('./data');
 }
-fs.writeFileSync(`./data/new-${ADDRESS}.json`, JSON.stringify(balanceHistory, null, 2), 'utf-8');
+const fileName = `./data/${ADDRESS}.json`
+fs.writeFileSync(fileName, JSON.stringify(balanceHistory, null, 2), 'utf-8');
+console.log("Balance history saved to", fileName);
 
 
